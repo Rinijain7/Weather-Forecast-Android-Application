ae8c7a68d2f093a817a5f795f8a0273ad08a974a
Rini Jain <Rini@Rinis-MacBook-Pro.local>
2016-01-22 20:40:54 -0800

    added wfa

---
+++ WeatherForecastAndroid/app/src 2/main/java/weather/rini/com/weatherforecast/model/LatLong.java
@@ -0,0 +1,26 @@
+package weather.rini.com.weatherforecast.model;
+
+/**
+ * Created by Rini on 11/22/2015.
+ */
+public class LatLong {
+    Float lat;
+    Float lng;
+
+    public Float getLng() {
+        return lng;
+    }
+
+    public void setLng(Float lng) {
+        this.lng = lng;
+    }
+
+    public Float getLat() {
+
+        return lat;
+    }
+
+    public void setLat(Float lat) {
+        this.lat = lat;
+    }
+}
